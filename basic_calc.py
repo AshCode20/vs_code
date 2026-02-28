47a7fd44b327224f0a40ab4713007de8f3a0e884
AshCode20 <punyashrinag.sonu@gmail.com>
2026-02-28 22:07:04 +0530

    Updated code today

--- basic_calc.py
+++ basic_calc.py
@@ -1,3 +1,5 @@
+import math 
+
 #input numbers
 input_data = input("Enter the numbers with space in between:")
 converted = []
@@ -6,21 +8,58 @@ numbers = input_data.split()
 #convert the numbers into float
 for value in numbers:
     converted.append(float(value))
-print(converted)
+
 
 if len(converted) == 0:
     print ("Your list is empty. ")
 
 
 #calculating mean
-def mean_calc(converted): 
+def mean_calc(converted,): 
     value = 0 
     total=0
-    while(value <= len(converted)):
+    for value in converted:
         total = value + total
         value += 1
         
     mean = total / len(converted)
     return mean
 
-print(mean_calc(converted))
+
+print("The mean of the numbers are:",mean_calc(converted))
+mean_value=mean_calc(converted )
+
+#calculating sample variance
+def sample_variance(converted, mean_value):
+    total = 0
+    for value in converted:
+        var = (value-mean_value)**2
+        total = var + total
+    return total
+loop_var = sample_variance(converted, mean_value)
+if len(converted) <= 1:
+    print("sample variance not defined for samples with size less than 2")
+else:
+    variance = (1/(len(converted)-1))*loop_var
+    print("The sample variance of the numbers are: " ,variance)
+
+#calculate standard deviation
+def std_dev(converted, variance):
+    standard_deviation = math.sqrt(variance)
+    return standard_deviation
+print("The standard deviation of the numbers is: " ,std_dev(converted, variance))
+
+#calculate median
+def median(converted):
+    sorted_list = sorted(converted)
+
+    n = len(sorted_list)
+    if n%2 == 0:
+        first_value = n//2
+        second_value = n//2 - 1
+        median_value = (sorted_list[first_value] + sorted_list[second_value]) / 2
+    else:
+        value = n//2 
+        median_value = sorted_list[value]
+    return median_value
+print("The median of the numbers is: ",median(converted))
