981755d03756e8d3d3f4fec9b700412f0be4ba8c
AshCode20 <punyashrinag.sonu@gmail.com>
2026-03-05 21:39:28 +0530

    not perfect yet

--- bio_sem4(lab).py
+++ bio_sem4(lab).py
@@ -1,4 +1,36 @@
-from Bio import Entrez
+from Bio import Entrez # entrez connects to ncbi
 Entrez.email = "punyashrinag.sonu@gmail.com"
 Gene = Entrez.efetch(db = "nucleotide", id = "EU498707", rettype = "fasta")
 print(Gene.read())
+
+from Bio import Entrez
+Entrez.email = "punyashrinag.sonu@gmail.com"
+info = Entrez.esearch(db="pubmed", term= "Malaria")
+print(Entrez.read(info))
+
+# DNA / RNA / Protein Sequence Generation
+from Bio.SeqRecord import SeqRecord
+from Bio.Seq import Seq
+record1 = SeqRecord(Seq("VIVEKKESHRIREVA"),id = "WS_1111.1", name="VIVEKPROTEIN", description="toxic protein")
+record2 = SeqRecord(Seq("ATGCATGCATGC"), id = "TEST001", name="VIVEKGENE", description="toxic gene")
+record3 = SeqRecord(Seq("AUGCAUGCAUGC"), id = "TEST002", name="VIVEKRNA", description="toxic RNA")
+print(record1.format("fasta"))
+print(record2.format("fasta"))
+print(record3.format("fasta"))
+
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio.Seq import MutableSeq
+my_dna1 = Seq("ACGTTT")
+print("my_dna1:", my_dna1)
+#my_dna1[0] = "T"
+#print(my_dna1)
+my_dna2 = MutableSeq("ACGTTT")
+print("my_dna2:", my_dna2)
+my_dna2[0]= "T"
+print("my_dna2_MutableSeq:", my_dna2)
+
+
+record1 = SeqRecord(Seq("VIVEKKESHRIREVA"), id = "WS_1111.1", name="VIVEKGENE", description="toxic protein")
+print(record1.format("fasta"))
+print(len(record1))
--- biolab_myedits.py
+++ biolab_myedits.py
@@ -1,7 +1,73 @@
-from Bio import Entrez # entrez connects to ncbi
+"""from Bio import Entrez # entrez connects to ncbi
 Entrez.email = "punyashrinag.sonu@gmail.com"
 Gene1 = Entrez.efetch(db = "nucleotide", id = "EU498707", rettype = "fasta")
     # efetch - fetches data [db:database name, id:accession no, rettype: format type(eg fasta), retmode: format mode(eg text, xml), restart: starting record, retmax: number of records]
 print(Gene1.read())
 Gene2 = Entrez.esearch(db = "nucleotide", term = "newborn jaundice", retmax = "3")
-print(Gene2.read())
+print(Gene2.read())"""
+
+# Program demonstrating DNA sequence editing using MutableSeq in Biopython
+from Bio.Seq import MutableSeq
+
+
+sequence = MutableSeq("ATTGACTTGCATCGCATCGACTCGACTCGACTACG")
+print("original sequence:")
+print(sequence)
+
+#inserting a nucleotide
+def baseSubs_nucl(sequence):
+    print("Performing base substitution:")
+    position = int(input ("enter the position"))
+    new_base = input("enter the new base")
+    sequence[position - 1] = new_base.upper()
+    return sequence
+print(baseSubs_nucl(sequence))
+
+#insertion
+def insert_nucl(sequence):
+    print("Performing insertion:")
+    position = int(input ("enter the position"))
+    new_base = input("enter the new base")
+    sequence.insert(position, new_base.upper())
+    return sequence
+print(insert_nucl(sequence))
+
+#deletion
+def deletion_nucl(sequence):
+    print("Performing deletion")
+    position = int(input ("enter the position"))
+    new_sequence = sequence[ : position] + sequence[position+1 : ]
+    return new_sequence
+print(deletion_nucl(sequence))
+
+def complement_nucl(sequence):
+    print("Finding complements:")
+    complement_sequence = ""
+    for base in sequence:
+        if base == 'A':
+            complement_sequence = complement_sequence + 'T'
+        elif base == 'T':
+            complement_sequence = complement_sequence + 'A'
+        elif base == 'G':
+            complement_sequence = complement_sequence + 'C'
+        else:
+            complement_sequence = complement_sequence + 'G'
+    return complement_sequence
+print(complement_nucl(sequence))
+
+def reverse_complement(sequence):
+    print("Reverse complement:")
+    reverse = sequence[ : : -1]
+    return reverse
+print(reverse_complement(complement_nucl(sequence)))
+
+
+
+
+
+
+
+    
+
+
+
